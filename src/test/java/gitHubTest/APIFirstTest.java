1b8e8729ad4b788dc0aa52c0599d74d4f83c0db7
o-yelkoff <o.yelkoff@gmail.com>
2023-07-18 20:04:13 +0300

    Homework10

--- src/test/java/gitHubTest/APIFirstTest.java
+++ src/test/java/gitHubTest/APIFirstTest.java
@@ -48,7 +48,7 @@ public class APIFirstTest extends BaseAPITest {
     }
 
     @Test
-    public void checkJsonSchema() {
+    public void checkJsonSchema2() {
         given()
                 .spec(requestSpecification)
                 .contentType(ContentType.JSON)
